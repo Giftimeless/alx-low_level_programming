1403a3cce60967c4cde2351848101225c66c966f
Giftimeless <giftjohn788@gmail.com>
2022-09-09 16:04:43 -0700

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char gift;
+	
+	for (gift = 'a'; gift <= 'z'; gift++)
+		putchar(gift);
+	putchar('\n');
+	return (0);
+}
+
