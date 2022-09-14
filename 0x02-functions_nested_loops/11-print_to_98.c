4497fae497319dc1a7ca4c3f7249b339c6be5b1a
Giftimeless <giftjohn788@gmail.com>
2022-09-14 05:37:59 -0700

    print number up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - function that prints all naturalbnumbers from n to 98
+ * user input's nimber prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <=98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",")
+			}
+			n++
+		}
+	}
+	else if (n> 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
