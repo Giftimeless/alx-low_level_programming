b80c1783807c4928c8c1ac5e3dbf7202e175f07e
Giftimeless <giftjohn788@gmail.com>
2022-09-14 05:52:13 -0700

    print number up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,17 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <=98)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n++;
 		}
 	}
-	else if (n> 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
