4943ea49db14c0ecc3bf5158ca1490a13f6cfae3
Giftimeless <giftjohn788@gmail.com>
2022-09-16 01:33:56 -0700

    Main head

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main-tests function that prints if integer is positive or negative
+ *
  * Return: 0
  */
 
@@ -10,6 +11,8 @@ int main(void)
 	int i;
 
 	i = 0;
-		positive_or_negative(i);
-		return (0);
+	positive_or_negative(i);
+
+	return (0);
+
 }
