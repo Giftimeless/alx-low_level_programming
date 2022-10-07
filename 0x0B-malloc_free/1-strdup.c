323738ea409dce43d066aa2006d07bd0088ad4a6
Giftimeless <giftjohn788@gmail.com>
2022-10-07 15:25:39 -0700

    function that returns a pointer to a newly allocated space in memory contain in a string in a given parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _strlen - count array
- * @s: array of elements
- * Return:
+ * @s: array of an element
+ * Return: 1
  */
 
 int _strlen(char *s)
 {
 	unsigned int i;
-	
+
 	i = 0;
-	while (s[i] !='\0')/** Count string */
+	while (s[i] != '\0') /*count string character*/
 	{
 		i++;
 	}
@@ -19,10 +19,10 @@ int _strlen(char *s)
 }
 
 /**
- * _strcpy - copy arrays
+ * _strcpy - copy string array
  * @src: array of elements
- * @dest: array destinatioon
- * Return : dest
+ * @dest: destination array
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -31,4 +31,39 @@ char *_strcpy(char *dest, char *src)
 
 	while (src[i] != '\0')
 	{
-		dest[i] i
+		dest[i] = src[i];
+			i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * *_strdup - array for print of a string
+ * @str: array of elements
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) + 1;
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+
+	_strcpy(dst, str);
+	return (dst);
+}
